60ba19815fae9a481027d5da58b4eebeccba009a
niloptto002 <nilopttobarman@gmail.com>
2026-04-19 23:09:12 +0600

    Update VitalLinkFrame.java
    Implemented Iterator Pattern in VitalLinkFrame.java

--- VitalLinkFrame.java
+++ VitalLinkFrame.java
@@ -1,5 +1,37 @@
 import javax.swing.*;
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.util.List;
+
+interface DonorIterator {
+    boolean hasNext();
+    Donor next();
+}
+
+class DonorCollection {
+    private List<Donor> donors;
+
+    public DonorCollection(List<Donor> donors) {
+        this.donors = donors;
+    }
+
+    public DonorIterator createIterator() {
+        return new DonorListIterator();
+    }
+
+    private class DonorListIterator implements DonorIterator {
+        private int index = 0;
+
+        public boolean hasNext() {
+            return index < donors.size();
+        }
+
+        public Donor next() {
+            return donors.get(index++);
+        }
+    }
+}
 
 public class VitalLinkFrame extends JPanel {
     private JTabbedPane tabs;
@@ -11,11 +43,13 @@ public class VitalLinkFrame extends JPanel {
         setLayout(new BorderLayout());
 
         JPanel topPanel = new JPanel(new BorderLayout());
-        topPanel.setBackground(new Color(220, 20, 60)); 
+        topPanel.setBackground(new Color(220, 20, 60));
+
         JLabel lbl = new JLabel("VitalLink");
         lbl.setFont(new Font("Arial", Font.BOLD, 28));
         lbl.setForeground(Color.WHITE);
         lbl.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
         topPanel.add(lbl, BorderLayout.WEST);
         add(topPanel, BorderLayout.NORTH);
 
@@ -31,9 +65,19 @@ public class VitalLinkFrame extends JPanel {
         tabs.addTab("All Donors", allDonorsPanel);
 
         add(tabs, BorderLayout.CENTER);
+
+        DonorCollection collection =
+                new DonorCollection(DonorManager.getInstance().getAllDonors());
+
+        DonorIterator iterator = collection.createIterator();
+        while (iterator.hasNext()) {
+            Donor d = iterator.next();
+        }
     }
 
-    public JPanel getMainPanel() { return this; }
+    public JPanel getMainPanel() {
+        return this;
+    }
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
